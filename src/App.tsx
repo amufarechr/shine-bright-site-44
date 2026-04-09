1d0ba1ddc87d01b463ab9041bd08dbbbe7f039ea
amufarechr <amufarechr@gmail.com>
2026-04-09 10:25:47 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import Consultoria from "./pages/Consultoria.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import Climatizacion from "./pages/Climatizacion.tsx";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/consultoria" element={<Consultoria />} />
+          <Route path="/soluciones/climatizacion" element={<Climatizacion />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
